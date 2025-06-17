4f3a0b5e8ddb5a0f27e5e8f6dcdd9c4b6d90e79f
Bruno Costa <brunogomesc@outlook.com.br>
2025-06-17 18:10:56 -0300

    criaçaõ do projeto, execução de migrations e alteração de titulos

--- src/DinheiroSobControle.Blazor/Menus/DinheiroSobControleMenus.cs
+++ src/DinheiroSobControle.Blazor/Menus/DinheiroSobControleMenus.cs
@@ -2,8 +2,8 @@
 
 public class DinheiroSobControleMenus
 {
-    private const string Prefix = "DinheiroSobControle";
-    public const string Home = Prefix + ".Home";
+    private const string Prefix = "Dinheiro Sob Controle";
+    public const string Home = Prefix + " - Home";
 
     //Add your menu items here...
 
